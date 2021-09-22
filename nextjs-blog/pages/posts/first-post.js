7b7cf264d4094684a5a4546218e8ddc4f15bbe7e
sato064 <satoberu@gmail.com>
2021-09-22 15:52:36 +0900

    first commit

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Link from "next/dist/client/link"
+import Head from "next/dist/shared/lib/head"
+import Layout from "../../components/layout"
+
+
+export default function FirstPost() {
+    return (
+    <Layout>
+        <Head>
+            <title>First post</title>
+        </Head>
+    <h1>First Post</h1>
+    <h2>
+        <Link href="/">
+            <a>Back to home</a>
+        </Link>
+    </h2>
+    </Layout>
+        )
+}
